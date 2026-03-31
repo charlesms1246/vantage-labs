6e377aceaaab3d02594ff48f0fae4e49fdd8ce86
charlesms1246 <charlesms2006@gmail.com>
2026-03-31 12:32:56 +0530

    feat: update Jest configuration and add integration test scripts

--- backend/jest.integration.config.js
+++ backend/jest.integration.config.js
@@ -6,8 +6,12 @@ module.exports = {
   roots: ['<rootDir>/tests/contract-integration'],
   testMatch: ['**/*.test.ts'],
   testTimeout: 120000,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
   moduleNameMapper: {
     '^uuid$': '<rootDir>/tests/__mocks__/uuid.js',
   },
-  runInBand: true,
 };
