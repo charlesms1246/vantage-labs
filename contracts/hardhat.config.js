07b37507a28ce9ec621d379513ed185afc8a4ed6
charlesms1246 <charlesms2006@gmail.com>
2026-03-31 07:46:32 +0530

    init: contracts folder with files

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "cancun",
+    },
+  },
+  networks: {
+    filecoinCalibnet: {
+      url: "https://api.calibration.node.glif.io/rpc/v1",
+      chainId: 314159,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    flowTestnet: {
+      url: "https://testnet.evm.nodes.onflow.org",
+      chainId: 545,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
